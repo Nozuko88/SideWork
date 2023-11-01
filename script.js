8eec7a5719c5fb0561fb9fc7f03beb3b90a61d97
Zuko <nozukosazimajola@gmail.com>
2023-11-01 14:35:01 +0200

    hangman

---
+++ script.js
@@ -0,0 +1,64 @@
+const letterContainer = document.getElementById("letter-container");
+const optionsContainer = document.getElementById("options-container");
+const userInputSection = document.getElementById("user-input-section");
+const newGameContainer = document.getElementById("new-game-container");
+const newGameButton = document.getElementById("new-game-button");
+const canvas = document.getElementById("canvas");
+const resultText = document.getElementById("result-text");
+
+
+let options = {
+    fruits: [
+        "Apple",
+        "Blueberry",
+        "Mandarin",
+        "Pineapple",
+        "Pomegranate",
+        "Watermelon",
+    ],
+    animals: ["Hedgehog", "Rhinoceros", "Squirrel", "Panther", "Walrus", "Zebra"],
+    countries: [
+        "India",
+        "Hungary",
+        "Kyrgyzstan",
+        "Switzerland",
+        "Zimbabwe",
+        "Diminica",
+    ],
+};
+
+let winCount = 0;
+let count = 0;
+
+let chosenWord = "";
+
+const displayOptions = () => {
+    optionsContainer.innerHTML += '<h3> Please Select An Option</h3>';
+    let buttonCon = document.createElement("div");
+    for (let value in options) {
+        buttonCon.innerHTML += `<button class="options" onclick="generateWord('${value}')">${value}</button>`;    
+    }        
+    optionsContainer.appendChild(buttonCon);
+};
+
+const blocker = () => {
+    let optionsButtons = document.querySelectorAll(".options");
+    let letterButtons = document.querySelectorAll(".letters");
+
+    optionsButtons.forEach((button) => {
+        button.disabled = true;
+    });
+
+    letterButtons.forEach((button) => {
+        button.disabled.true;
+    });
+    newGameContainer.classList.remove("hide");
+};
+
+const generate = (optionValue) => {
+    let optionsButtons = document.querySelectorAll(".options");
+
+    optionsButtons.forEach((button) => {
+        
+    })
+}
